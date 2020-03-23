c5d8932d591c7876983eea9930864b44b602d7bb
neri78 <neri78@live.jp>
2020-03-23 23:03:08 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Twilio-Voice-AiTalkWebAPI
 Twilio Programmable Voiceと、AiTalk Web APIを連携させるサンプルプログラムです。
+
+## サンプルの実行方法
+
+サンプルの実行には、[AiTalk WebAPI](https://www.ai-j.jp/cloud/webapi/)の[アカウント](https://aitalk-webapi-trial.aitalk.jp/)と[Twilio](https://www.twilio.com/ja)の[アカウント]()が必要です。  
+
+それぞれのアカウントを作成後<code>.env.sample</code>ファイルをコピーし<code>.env</code>とリネームします。<code>.env</code>ファイルにはAiTalk Web APIのエンドポイントやユーザー名、パスワード、このサンプルをホストするURLそれぞれの値を設定してください。
+
+```.envファイル
+AI_TALK_API_URL=AiTalk Web APIのエンドポイント（例 - https://api.example.com）
+AI_TALK_USERNAME=AiTalk Web APIのユーザー名（例 - userame）
+AI_TALK_PASSWORD=AiTalk Web APIのパスワード（例 - password）
+```
+
+続けて、下記のコマンドを実行し、サンプルプログラムを起動します。
+
+```プロジェクトの実行方法
+npm install
+npm start
+```
+
+ローカル環境でホストする場合は、[ngrok](https://ngrok.com/)などのツールを使い、生成されたURLを記録しておきます。（例 - https://daizen.ngrok.io)  
+このURLを[Twilioコンソール](https://jp.twilio.com/console)で取得した電話番号のWebhookに指定します。 __（例: https://daizen.ngrok.io<code>/incoming</code>）__
+![Twilio Voice Console](https://neri78.github.io/images/twilio-console/Twilio-Console-Voice.png)
+
+これで準備は完了です。Webhookを設定した番号に電話をかけてみましょう！
---
+++ index.js
@@ -0,0 +1,91 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const fs = require('fs');
+const got = require('got');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+// 生成したファイルを保存しておくフォルダ
+app.use(express.static(path.join(__dirname, 'voice_files')));
+
+// Twilioから着信リクエストを受け取るAPI
+app.post('/incoming', async (req, res) => {
+    let phoneNumber = req.body.From;
+    // 音声合成するテキストを設定。実際はAIエンジンなどから生成される。
+    const text = '今日はお電話ありがとう！エーアイトークとTwilioを連携した音声合成デモだよ！ あなたの電話番号は' + req.body.From + 'だよね？ぜひ、両方のサービスを試してみてね。';
+
+    // AiTalk Web API用のパラメーターを作成
+    // AiTalk Web APIでは最低限ユーザー名、パスワード、合成文字列、話者名が必要
+    // このサンプルではのぞみ（感情対応）、最大限の喜びを抑揚をつけてmp3で出力します。
+    const aiTalkOptions = {
+        'username' : process.env.AI_TALK_USERNAME,
+        'password' : process.env.AI_TALK_PASSWORD,
+        'text' : text,
+        'speaker_name' : 'nozomi_emo',
+        'style' : { 'j': '1.0'},
+        'range' : 2.00,
+        'ext' : 'mp3'
+    }
+
+    // リクエストヘッダ
+    var headers = {
+        'Content-Type' : 'application/x-www-form-urlencoded'
+    };
+    // リクエストオプション
+    var options = {
+        method: 'POST',
+        headers: headers,
+        form: aiTalkOptions,
+        encoding: 'binary'
+    };
+        try {
+        //AiTalk Web APIにリクエスト
+        const response = await got(process.env.AI_TALK_API_URL, options);
+
+        if (response.statusCode === 200 ) {
+            
+            //現在のCallSidから出力ファイル名を生成
+            const outputFileName = req.body.CallSid + '.mp3';
+            // mp3ファイルを保存
+            await fs.promises.writeFile(
+                path.join('voice_files', outputFileName), response.body, 'binary');
+    
+            // TwiMLを初期化
+            const VoiceResponse = require('twilio').twiml.VoiceResponse;
+            const twiml = new VoiceResponse();
+ 
+            // 生成した音声を再生させる。
+            twiml.play(path.join(outputFileName));
+
+            //ヘッダを設定し、作成したTwiMLをレスポンスとして送信
+            res.writeHead(200, {'Content-Type': 'text/xml'});
+            res.end(twiml.toString());
+        }
+    } catch (error) {
+            console.dir(error);
+    }
+});  
+
+app.post('/statuschanged', async(req, res) => {
+    // 通話が完了した段階で該当ファイルを削除
+    if (req.body.CallStatus === "completed")
+    {
+        var filePath = 'voice_files/' + req.body.CallSid + '.mp3';
+        fs.exists(filePath, (result) =>{
+            if (result)
+            {
+                fs.unlink(filePath, (err)=>{
+                if (err) throw err;
+                console.log(filePath + 'を削除しました。');
+                });
+            }
+        });
+    }
+    res.writeHead(200, {'Content-Type': 'text/xml'});
+    res.end('OK');
+});
+
+app.listen(3000, () => console.log('Listening on port 3000'));
+
