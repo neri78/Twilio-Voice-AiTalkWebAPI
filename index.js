deb7ad434fc7e3ba1ead59604482492fd4f13c6a
neri78 <neri78@live.jp>
2020-03-27 15:13:45 +0900

    update code

--- index.js
+++ index.js
@@ -4,21 +4,24 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const fs = require('fs');
 const got = require('got');
+//着信に対して応答を返すVoice用TwiMLを作成するクラス
+const VoiceResponse = require('twilio').twiml.VoiceResponse;;
 
 const app = express();
+const VOICE_FOLDER_NAME = 'voice_files';
 app.use(bodyParser.urlencoded({ extended: true }));
-// 生成したファイルを保存しておくフォルダ
-app.use(express.static(path.join(__dirname, 'voice_files')));
+// 生成したファイルを保存しておくフォルダを設定
+app.use(express.static(path.join(__dirname, VOICE_FOLDER_NAME)));
 
 // Twilioから着信リクエストを受け取るAPI
 app.post('/incoming', async (req, res) => {
-    let phoneNumber = req.body.From;
+
     // 音声合成するテキストを設定。実際はAIエンジンなどから生成される。
     const text = '今日はお電話ありがとう！エーアイトークとTwilioを連携した音声合成デモだよ！ あなたの電話番号は' + req.body.From + 'だよね？ぜひ、両方のサービスを試してみてね。';
 
     // AiTalk Web API用のパラメーターを作成
     // AiTalk Web APIでは最低限ユーザー名、パスワード、合成文字列、話者名が必要
-    // このサンプルではのぞみ（感情対応）、最大限の喜びを抑揚をつけてmp3で出力します。
+    // このサンプルではのぞみ（感情対応）、最大限の喜びを抑揚をつけてmp3で出力
     const aiTalkOptions = {
         'username' : process.env.AI_TALK_USERNAME,
         'password' : process.env.AI_TALK_PASSWORD,
@@ -29,36 +32,28 @@ app.post('/incoming', async (req, res) => {
         'ext' : 'mp3'
     }
 
-    // リクエストヘッダ
-    var headers = {
-        'Content-Type' : 'application/x-www-form-urlencoded'
-    };
-    // リクエストオプション
-    var options = {
-        method: 'POST',
-        headers: headers,
-        form: aiTalkOptions,
-        encoding: 'binary'
-    };
-        try {
+    try {
+        // リクエストオプション
+        const options = {
+            method: 'POST',
+            form: aiTalkOptions,
+            encoding: 'binary'
+        };
+
         //AiTalk Web APIにリクエスト
         const response = await got(process.env.AI_TALK_API_URL, options);
 
         if (response.statusCode === 200 ) {
-            
             //現在のCallSidから出力ファイル名を生成
             const outputFileName = req.body.CallSid + '.mp3';
             // mp3ファイルを保存
             await fs.promises.writeFile(
-                path.join('voice_files', outputFileName), response.body, 'binary');
+                path.join(VOICE_FOLDER_NAME, outputFileName), response.body, 'binary');
     
             // TwiMLを初期化
-            const VoiceResponse = require('twilio').twiml.VoiceResponse;
             const twiml = new VoiceResponse();
- 
             // 生成した音声を再生させる。
             twiml.play(path.join(outputFileName));
-
             //ヘッダを設定し、作成したTwiMLをレスポンスとして送信
             res.writeHead(200, {'Content-Type': 'text/xml'});
             res.end(twiml.toString());
